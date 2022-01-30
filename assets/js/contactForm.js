7ac81d31d8dc7fa375dd24639cf76b357174c343
Fidan Mirizade <fidan.mirizadaa@gmail.com>
2022-01-30 23:44:35 +0400

    connect contact and terminal form with server

---
+++ assets/js/contactForm.js
@@ -0,0 +1,70 @@
+/**
+ * @description      :
+ * @author           : hp
+ * @group            :
+ * @created          : 22/01/2022 - 12:47:29
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 22/01/2022
+ * - Author          : hp
+ * - Modification    :
+ **/
+const fullName = document.querySelector('#contactForm #fullName')
+const applicationReason = document.querySelector(
+  '#contactForm #applicationReason'
+)
+const email = document.querySelector('#contactForm #email')
+const phone = document.querySelector('#contactForm #phone')
+const note = document.querySelector('#contactForm #note')
+const submitBtn = document.querySelector('#contactForm button')
+
+async function contactFormFunc () {
+  submitBtn.classList.add('disable')
+  const formData = {
+    fullName: fullName?.value,
+    applicationReason:
+      applicationReason?.options[applicationReason.selectedIndex].text,
+    email: email?.value,
+    phone: `+994${phone?.value}`,
+    note: note?.value
+  }
+
+  console.log(formData)
+
+  const response = await fetch(
+    'http://157.90.55.174/api/ContactForm/InsertContactForm',
+    {
+      method: 'post',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-type': 'application/json; charset=utf-8'
+      }
+    }
+  )
+    .then(res => res.json())
+    .then(data => data)
+
+  if (!response.success) {
+    alert('Uğursuz əməliyyat!')
+  }
+  submitBtn.classList.remove('disable')
+}
+
+const contactForms = document.querySelectorAll('.requires-validation')
+Array.from(contactForms).forEach(function (form) {
+  form.addEventListener(
+    'submit',
+    function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      if (!form.checkValidity()) {
+      } else {
+        contactFormFunc()
+      }
+
+      form.classList.add('was-validated')
+    },
+    false
+  )
+})
---
+++ assets/js/terminalForm.js
@@ -0,0 +1,63 @@
+/**
+ * @description      :
+ * @author           : hp
+ * @group            :
+ * @created          : 22/01/2022 - 12:47:29
+ *
+ * MODIFICATION LOG
+ * - Version         : 1.0.0
+ * - Date            : 22/01/2022
+ * - Author          : hp
+ * - Modification    :
+ **/
+const terminalFormEmail = document.querySelector('#terminalForm #email')
+const terminalFormPhone = document.querySelector('#terminalForm #phone')
+const submitBtn = document.querySelector('#terminalForm button')
+
+async function terminalFormFunc () {
+  submitBtn.classList.add('disable')
+  const formData = {
+    email: terminalFormEmail?.value,
+    phone: `+994${terminalFormPhone?.value}`
+  }
+
+  const response = await fetch(
+    'http://157.90.55.174/api/ContactForm/InsertRequestTerminal',
+    {
+      method: 'post',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-type': 'application/json; charset=utf-8'
+      }
+    }
+  )
+    .then(res => res.json())
+    .then(data => data)
+
+  submitBtn.classList.remove('disable')
+
+  if (response.success) {
+    document.querySelector('.terminal_form .form_body').style.display = 'none'
+    document.querySelector('.terminal_form .success').style.display = 'flex'
+  } else {
+    alert('Uğursuz əməliyyat!')
+  }
+}
+
+const forms = document.querySelectorAll('.requires-validation')
+Array.from(forms).forEach(function (form) {
+  form.addEventListener(
+    'submit',
+    function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      if (!form.checkValidity()) {
+      } else {
+        terminalFormFunc()
+      }
+
+      form.classList.add('was-validated')
+    },
+    false
+  )
+})
